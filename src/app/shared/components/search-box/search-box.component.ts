856019dfd993d84ba3b546a71a6ab852f69e9c76
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-05-16 20:45:46 -0500

    destruir suscripcion

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,13 +1,21 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Subject, debounceTime } from 'rxjs';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { Subject, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styles: [],
 })
-export class SearchBoxComponent implements OnInit {
+export class SearchBoxComponent implements OnInit, OnDestroy {
   private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSuscription?: Subscription;
 
   @Input()
   public placeholder: string = '';
@@ -20,9 +28,16 @@ export class SearchBoxComponent implements OnInit {
 
   ngOnInit(): void {
     // observable que me da un delay de 1 segundo para darle espera a que escriba mas caracteres
-    this.debouncer.pipe(debounceTime(300)).subscribe((value) => {
-      this.onDebounce.emit(value);
-    });
+    this.debouncerSuscription = this.debouncer
+      .pipe(debounceTime(300))
+      .subscribe((value) => {
+        this.onDebounce.emit(value);
+      });
+  }
+
+  ngOnDestroy(): void {
+    //! elimino la suscripcion del debouncer (siempre se debe hacer)
+    this.debouncerSuscription?.unsubscribe();
   }
 
   emitValue(value: string): void {
